5693462ffaecab473a2490f1a8cb632d518acfbe
Maddison Wells <maddisonwells@Maddisons-MacBook-Pro.local>
2024-04-08 23:12:00 +1000

    finished tutorial

--- index.js
+++ index.js
@@ -30,6 +30,42 @@ const resolvers = {
       return db.reviews.filter((review) => review.game_id === parent.id);
     },
   },
+  Author: {
+    reviews(parent) {
+      return db.reviews.filter((review) => review.author_id === parent.id);
+    },
+  },
+  Review: {
+    author(parent) {
+      return db.authors.find((a) => a.id === parent.author_id);
+    },
+    game(parent) {
+      return db.games.find((g) => g.id === parent.game_id);
+    },
+  },
+  Mutation: {
+    deleteGame(_, args) {
+      db.games = db.games.filter((g) => g.id !== args.id);
+      return db.games;
+    },
+    addGame(_, args) {
+      let game = {
+        ...args.game,
+        id: Math.floor(Math.random() * 10000).toString(),
+      };
+      db.games.push(game);
+      return game;
+    },
+    updateGame(_, args) {
+      db.games = db.games.map((g) => {
+        if (g.id === args.id) {
+          return { ...g, ...args.edits };
+        }
+        return g;
+      });
+      return db.games.find((g) => g.id === args.id);
+    },
+  },
 };
 
 const server = new ApolloServer({
